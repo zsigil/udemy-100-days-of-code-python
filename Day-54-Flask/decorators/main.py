9ba15c8b80bdcdf16f0ebc99c12ea1fa89493943
Zsigil <chickpox@freemail.hu>
2023-06-26 17:18:02 +0200

    decorators and flask

---
+++ Day-54-Flask/decorators/main.py
@@ -0,0 +1,28 @@
+import time
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(func):
+    def wrapper():
+        start_time = time.time()
+        func()
+        end_time = time.time()
+        print(func.__name__, end_time-start_time)
+    return wrapper
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+
+fast_function()
+slow_function()
